f0e2957b9cb44f213a605e9a03cb393af753c049
Nguyen Viet Bach <74226317+ngvbach2000@users.noreply.github.com>
2022-08-02 10:20:28 +0700

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,2 +1,8 @@
-def setup_function():
-    print("Pass")
+from locust import HttpUser, task, between
+
+class WebsiteTestUser(HttpUser):
+    wait_time = between(0.5, 4.0)
+
+    @task(1)
+    def test(self):
+        self.client.post("https://udacity-c2.scm.azurewebsites.net/")
